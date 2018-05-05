19f5dd5ab30a9e4f38cb6ddf25aff8e9178eb508
FacuEt <facurawson@gmail.com>
2018-05-05 18:26:25 -0300

    add firebase to project

--- app/src/main/java/com/ar/tdp2fiuba/hoycomo/model/request/FacebookLogInRequest.java
+++ app/src/main/java/com/ar/tdp2fiuba/hoycomo/model/request/FacebookLogInRequest.java
@@ -1,13 +1,21 @@
 package com.ar.tdp2fiuba.hoycomo.model.request;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
 public class FacebookLogInRequest {
     private String accessToken;
+    private String firebaseToken;
 
     public FacebookLogInRequest(String accessToken) {
         this.accessToken = accessToken;
+        this.firebaseToken = FirebaseInstanceId.getInstance().getToken();
     }
 
     public String getAccessToken() {
         return accessToken;
+    }
+
+    public String getFirebaseId() {
+        return firebaseToken;
     }
 }

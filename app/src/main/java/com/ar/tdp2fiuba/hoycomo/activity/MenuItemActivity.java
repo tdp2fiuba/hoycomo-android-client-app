a139f72d0fc8f05343ff8971fcd13ed202fb9dfd
juanmanuel.bouvier <juanmanuel.bouvier@almundo.com>
2018-05-07 10:54:20 -0300

    FIX: Removed home menu buttons from toolbar in MenuItemActivity.

--- app/src/main/java/com/ar/tdp2fiuba/hoycomo/activity/MenuItemActivity.java
+++ app/src/main/java/com/ar/tdp2fiuba/hoycomo/activity/MenuItemActivity.java
@@ -83,23 +83,6 @@ public class MenuItemActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void addToMyOrder(View v) {
         Log.d(TAG, "MenuItem with ID " + mMenuItem.getId() + " added to My Order");
         Order myCurrentOrder = OrderService.getMyCurrentOrder();
